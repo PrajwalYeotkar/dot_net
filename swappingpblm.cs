05125a07dd7efbd21728d276cad9cb5a2554155a
Prajwal Yeotkar <prajwalyeotkar@gmail.com>
2022-08-04 14:18:00 +0530

    swapping correction made

--- swappingpblm.cs
+++ swappingpblm.cs
@@ -1,20 +1,20 @@
 using System;
 using System.Text;
 namespace Swapping{
-    class problem{
+    class Problem{
         static void Main(string[] args){
-            string x = "first string";
-            string y = "second string";
-            swapkey S1 = new swapkey();
-            S1.swapmeth(ref x,ref y);
-            Console.WriteLine("The Value of x after swapping: " + x);
-            Console.WriteLine("The Value of y after swapping: " + y);
+            string word1 = "first string";
+            string word2 = "second string";
+            swapkey swapkey1 = new swapkey();
+            swapkey1.swapmeth(ref word1,ref word2);
+            Console.WriteLine("The Value of x after swapping: " + word1);
+            Console.WriteLine("The Value of y after swapping: " + word2);
         }
     }
     class swapkey{
-        public void swapmeth(ref string p,ref string q){
-            p = "second string";
-            q = "first string";
+        public void swapmeth(ref string string1,ref string string2){
+            string1 = "second string";
+            string2 = "first string";
         }
 
     }
